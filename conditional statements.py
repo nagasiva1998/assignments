7f1a8e1dfac3eb5a9c9ee97816971809837993f4
Nagasiva Reddy <nagasivareddy.marolix@gmail.com>
2023-11-15 09:55:29 +0530

    3rd commit

---
+++ conditional statements.py
@@ -0,0 +1,35 @@
+# 1.write a pro to find the biggest of given 2 numbers from the keyboard
+# using if condition statement
+a=int(input("enter the 1st value :"))
+b=int(input("enter the 2nd value :"))
+if a<b:
+    print("a is not more than b")
+else:
+    print("a is more than b")
+
+c=int(input("enter the value :"))
+if c==1:
+        print("one")
+elif c==2:
+        print("two")  
+elif c==3:
+     print("three")
+elif c==4:
+      print("four")
+elif c==5:
+      print("five")
+else:
+      print("not in list")
+
+
+# username, password
+name=" "
+password=" "
+name=input("enter the user name :")
+password=int(input("enter the password :"))
+if (name=='nagasiva')and(password==123456):
+      print("acces granted")
+else:
+      print("acces denied")
+
+
