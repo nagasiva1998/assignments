ce24de192b58578bc056ddf364a6c08e09a30f4b
Nagasiva Reddy <nagasivareddy.marolix@gmail.com>
2023-12-14 14:10:34 +0530

    13-12-2023 assignment

---
+++ 13-12-2023 assignment.py
@@ -0,0 +1,23 @@
+
+f(3,5)
+
+
+f(10,20,30,40)
+
+
+f(11,6,arg4=1000)
+
+
+f(arg4=6,arg1=7,arg2=9)
+
+
+# f(arg3=90,arg4=80,10,50)    SyntaxError: positional argument follows keyword argument
+
+
+#f(6,8,arg2=89)  got multiple values arg2
+
+
+#f(4,5,arg3=7,arg5=78)  TypeError: f() got an unexpected keyword argument 'arg5' 
+
+
+#f()   missing 2 required positional arguments: 'arg1' and 'arg2'
